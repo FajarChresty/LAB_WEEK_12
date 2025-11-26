27b28e32dd25a8c44c12060749e686248428c89c
FajarChresty <164896797+FajarChresty@users.noreply.github.com>
2025-11-26 15:02:14 +0700

    Commit No. 2 – Building app with coroutines

--- LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,30 @@
-package com.example.test_lab_week_12
+package com.example.test_lab_week_12.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
 
+    private val apiKey = "16cbedfc1d33d08ef90e77cc9daa3e1e"
+
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception: Exception) {
+            errorLiveData.postValue(
+                "An error occurred: ${exception.message}"
+            )
+        }
+    }
 }
--- LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ LAB_WEEK_12-master/app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,7 +1,25 @@
-package com.example.test_lab_week_12
+package com.example.test_lab_week_12.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_12.model.Movie
+import com.example.test_lab_week_12.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
 
+    init {
+        fetchPopularMovies()
+    }
+
+    val popularMovies: LiveData<List<Movie>> = repository.movies
+    val error: LiveData<String> = repository.error
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.fetchMovies()
+        }
+    }
 }
